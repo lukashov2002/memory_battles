1f07ca9e683890db00e1e7d5cae57414fb91bb15
mrhedgehogmr <58913658+mrhedgehogmr@users.noreply.github.com>
2019-12-18 19:56:01 +0000

    Add files via upload
    Not random algorithm for first player

--- player_one.py
+++ player_one.py
@@ -2,9 +2,31 @@ import player
 import random
 
 
-def act(cell: player.Cell, step: int):
-    where = ['d', 'u', 'l', 'r']
-    cell.upgr_order = 'hhhhh'
-    if cell.energy >= 10*(2**(cell.num_to_upgrade - 1)):
-        cell.upgrade_health()
-    cell.move(where[random.randint(0, 3)])
+def act(cell: player.Cell, blue_zone: int):
+    pos = cell.position
+    if pos[0] == 10:
+        cell.move('d')
+    elif pos[0] == -10:
+        cell.move('u')
+    if pos[1] == 10:
+        cell.move('l')
+    elif pos[0] == -10:
+        cell.move('r')
+    elif pos[0] + 1 >= blue_zone:
+        cell.move('d')
+    elif -pos[0] + 1 >= blue_zone:
+        cell.move('u')
+    elif pos[1] + 1 >= blue_zone:
+        cell.move('l')
+    elif -pos[1] + 1 >= blue_zone:
+        cell.move('r')
+    else:
+        where = ['d', 'u', 'l', 'r']
+        cell.move(where[random.randint(0, 3)])
+
+    if blue_zone % 2 == 0:
+        if cell.energy > 3 * 2 ** (cell.fight_level - 1):
+            cell.upgrade_force
+    else:
+        if cell.energy > 8:
+            cell.upgrade_health
